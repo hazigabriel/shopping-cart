94b0c4e6aabd0f54a444cc420ed933e1483cecf1
Gabriel Hazi <hazigabriel@gmail.com>
2022-01-27 01:51:13 +0200

    added cart page

---
+++ src/components/cart/cart.js
@@ -0,0 +1,72 @@
+import "./cart.css"
+import allProducts from "../../data/allProducts"
+import { FaTrashAlt } from 'react-icons/fa';
+
+
+function Checkout() {
+    return (
+        <div className="gridContainer">
+            <div className="gridItem">
+                <section className="cartWrapper">
+                    <div className="cartItems">
+                        <div className="cartItem">
+                            <img src={allProducts[12].image[0]}></img>
+                            <p>{allProducts[12].name}</p>
+                            <div className="productQuantity">
+                                <h2>-</h2>
+                                <h3>9</h3>
+                                <h2>+</h2>
+                            </div>
+                            <div className="productPrice">
+                                <p>$23</p>
+                            </div>
+                            <div className="productRemove"><FaTrashAlt /></div>
+                        </div>
+                        <div className="cartItem">
+                            <img src={allProducts[15].image[0]}></img>
+                            <p>{allProducts[15].name}</p>
+                            <div className="productQuantity">
+                                <h2>-</h2>
+                                <h3>9</h3>
+                                <h2>+</h2>
+                            </div>
+                            <div className="productPrice">
+                                <p>$23</p>
+                            </div>
+                            <div className="productRemove"><FaTrashAlt /></div>
+                        </div>
+                        <div className="cartItem">
+                            <img src={allProducts[12].image[0]}></img>
+                            <p>{allProducts[12].name}</p>
+                            <div className="productQuantity">
+                                <h2>-</h2>
+                                <h3>9</h3>
+                                <h2>+</h2>
+                            </div>
+                            <div className="productPrice">
+                                <p>$23</p>
+                            </div>
+                            <div className="productRemove"><FaTrashAlt /></div>
+                        </div>
+                        
+                        <div className="itemsTotal">
+                            <h2>Total:</h2>
+                            <h2>$342.12312</h2>
+                        </div>
+                    </div>
+
+                    <div className="promoCodeWrapper">
+                        <h3>Enter promo code</h3> 
+                        <input type="text"></input>
+                        <p className="submitPromoCode">Submit</p>
+                        <p className="validPromoCode">Valid code</p>
+                        <p className="invalidPromoCode">Invalid promo code</p>
+                    </div>               
+                </section>
+
+            </div>
+        </div>
+    )
+}
+
+export default Checkout
